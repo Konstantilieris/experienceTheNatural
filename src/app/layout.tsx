6fa20a5958369a459297319152db36df80b79f63
ANGELOS KONSTANTILIERIS <aggeloskonstantilieris@gmail.com>
2024-12-04 20:17:28 +0200

    mvp overscroll-none

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="no-scrollbar">
+    <html lang="en" className="no-scrollbar overscroll-none scroll-smooth">
       <body className={`${nunito.variable} h-full `}>
         <Nav />
         {children}
